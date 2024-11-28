98cd6be2175a3622495484782ae0f07fde725217
nicole yerushalmi <nicole.yerushalmi@post.ac.il>
2024-11-29 00:10:39 +0200

    prime test

--- Primes.java
+++ Primes.java
@@ -3,21 +3,23 @@ public class Primes {
         // Replace this statement with your code again
         int n = Integer.parseInt(args[0]);
         boolean [] prime_num = new boolean [n + 1];
-        int[] all_prime = new int [n];
         prime_num[0] = false;
         prime_num[1] = false;
         int s = 0;
+        int count = prime_num.length;
         for(int i = 2; i < prime_num.length; i++){
             prime_num[i] = true;
         }
         for(int k = 2; k < Math.sqrt(n); k ++){
             for(int t = k; t < prime_num.length; t++){
-                if (t % k == 0 && t != k) {
-                    prime_num[t] = false;
-                    System.out.println( " k = " + k + " t = " + t);
+                if (t % k == 0 && t != k && prime_num[t] != false) {
+                        count --;
+                        prime_num[t] = false;
+                    //System.out.println( " k = " + k + " t = " + t);
                 }
             }
         }
+        int[] all_prime = new int [count - 2];
         System.out.println("Prime numbers up to " + n + ":");
         for(int p = 0; p < prime_num.length; p++){
             if (prime_num[p] == true) {
@@ -26,7 +28,8 @@ public class Primes {
                 System.out.println(p);
             }
         }
-        double percent =((double) ((double)all_prime.length/prime_num.length)) * 100;
+        double percent =(int)(((double) ((double)all_prime.length/prime_num.length)) * 100);
+        //System.out.println("number of primes: " + all_prime.length);
         System.out.println("There are " + all_prime.length + " primes between 2 and " + n + " (" + percent + "% are primes)");
     }
 }
