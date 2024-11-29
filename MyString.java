6fadefc12b897600f9283a95d7af40b58b410762
nicole yerushalmi <nicole.yerushalmi@post.ac.il>
2024-11-29 08:16:03 +0200

    mystring try

--- MyString.java
+++ MyString.java
@@ -25,8 +25,8 @@ public class MyString {
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
         // Replace the following statement with your code
-        str1 = lowerCase(str1);
-        str2 = lowerCase(str2);
+        // str1 = lowerCase(str1);
+        // str2 = lowerCase(str2);
         boolean bool = true;
         if ((str2.length() == 0)) {
             return true;
