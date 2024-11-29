0d0d93bb5f0bf06d6a253b57eb8d551f2f71c32d
nicole yerushalmi <nicole.yerushalmi@post.ac.il>
2024-11-29 09:56:18 +0200

    keywordsdetector try

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -1,20 +1,21 @@
 public class KeywordsDetector {
     public static void main(String[] args) {
         String[] sentences = {
-            "Our product will transform the market",
-            "Programming is both painful and engaging",
-            "This has nothing to do with machine learning",
-            "We need to leverage our core competencies",
-            "Let's talk about data and algorithms",
-            "Chatbots are great but must be used carefully",
-            "This blockchain-based solution will disrupt the industry",
-            "The team showed great Synergy in the last project",
-            "Use simple words without hype and fluff",
-            "Our new technology presents a significant paradigm shift",
-            "Effective presentations must be clear, concise, and humble"
+                "Our product will transform the market",
+                "Programming is both painful and engaging",
+                "This has nothing to do with machine learning",
+                "We need to leverage our core competencies",
+                "Let's talk about data and algorithms",
+                "Chatbots are great but must be used carefully",
+                "This blockchain-based solution will disrupt the industry",
+                "The team showed great Synergy in the last project",
+                "Use simple words without hype and fluff",
+                "Our new technology presents a significant paradigm shift",
+                "Effective presentations must be clear, concise, and humble"
         };
-        // Some keywords that typically signal bullshit contents in business presentations 
-        String[] keywords = {"synergy", "disrupt", "leverage", "Paradigm", "transform"};
+        // Some keywords that typically signal bullshit contents in business
+        // presentations
+        String[] keywords = { "synergy", "disrupt", "leverage", "Paradigm", "transform" };
         detectAndPrint(sentences, keywords);
     }
 
@@ -22,5 +23,18 @@ public class KeywordsDetector {
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
         // Replace this comment with your code
+        String choose_word = "";
+        for (int i = 0; i < sentences.length; i++) { // go thrugh the sentences and brine 1
+            for (int t = 0; t < sentences[i].length(); t++) { // go throw the length of sentences and check number of string inside
+                choose_word = sentences[i].substring(sentences[i].indexOf(" "), sentences[i].indexOf(" "));
+                for (int k = 0; k < keywords.length; k++) { // go throw 
+                    if (choose_word == keywords[k]) {
+                        System.out.println(sentences[i]);
+                        break;
+                    }
+                }
+            }
+
+        }
     }
 }
