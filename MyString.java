d16d340f5fc83912540013c4d61c1afa5a444ea8
nicole yerushalmi <nicole.yerushalmi@post.ac.il>
2024-12-05 23:12:53 +0200

    my string try

--- MyString.java
+++ MyString.java
@@ -21,7 +21,7 @@ public class MyString {
         // Replace the following statement with your code
         for(int i = 0; i < str.length(); i++){
             char ch = str.charAt(i);
-            if ((str.charAt(i) > 'A' || str.charAt(i) < 'Z')) {
+            if ((str.charAt(i) > 'A' && str.charAt(i) < 'Z')) {
                  ch = (char)(ch + 32);
             }
             
